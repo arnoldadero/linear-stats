2aaa966215b6ed2c6097ffdd0434a2ee999d93bc
arnold.adero <arnold.adero@hotmail.com>
2024-10-15 13:39:00 +0300

    Fix: add error handling

--- regression/correlation.go
+++ regression/correlation.go
@@ -5,6 +5,10 @@ import "math"
 // CalculatePearsonCorrelation calculates the Pearson correlation coefficient
 func CalculatePearsonCorrelation(data []float64) float64 {
 	n := float64(len(data))
+	if n < 2 {
+		return 0 //not enough data points to calculate correlation coefficient
+    }
+
 	sumX, sumY, sumXY, sumXX, sumYY := 0.0, 0.0, 0.0, 0.0, 0.0
 
 	for i, y := range data {
@@ -19,6 +23,9 @@ func CalculatePearsonCorrelation(data []float64) float64 {
 	// Calculate Pearson correlation coefficient
 	numerator := n*sumXY - sumX*sumY
 	denominator := math.Sqrt((n*sumXX - sumX*sumX) * (n*sumYY - sumY*sumY))
+        if denominator == 0 {
+            return 0 // division by zero is not allowed
+        }
+    return numerator / denominator
 
-	return numerator / denominator
 }
